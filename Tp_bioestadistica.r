78bb24eb7ab2bb1dafa6ea9466f993787095cb4c
CARMELA CAPUTO <cmmgsc@gmail.com>
2025-06-10 18:16:54 -0300

    mini cambio

--- Tp_bioestadistica.r
+++ Tp_bioestadistica.r
@@ -1,4 +1,5 @@
-setwd(setwd("C:/Users/Equipo/Documents/Bioestadistica/TP"))
+path="C:/Users/Equipo/Documents/Bioestadistica/TP_bioestadistica"
+setwd(setwd(path))
 
-datos <- read.csv("C:/Users/Equipo/Documents/Bioestadistica/TP/kidney_disease.csv", sep=",",header= TRUE)
+datos <- read.csv("kidney_disease.csv", sep=",",header= TRUE)
 print(head(datos))
