b3fecc614a676e8761676b446a830b598540502c
Carmela Caputo <153946039+carmelagsc@users.noreply.github.com>
2025-06-10 18:12:07 -0300

    cargando dataset y un archivo R

---
+++ Tp_bioestadistica.r
@@ -0,0 +1,4 @@
+setwd(setwd("C:/Users/Equipo/Documents/Bioestadistica/TP"))
+
+datos <- read.csv("C:/Users/Equipo/Documents/Bioestadistica/TP/kidney_disease.csv", sep=",",header= TRUE)
+print(head(datos))
