5909ca8c5089a88188a5a5fae96c7a5683f4f589
CARMELA CAPUTO <cmmgsc@gmail.com>
2025-06-11 14:14:43 -0300

    agregue otras cosas del EDA

--- Tp_bioestadistica.r
+++ Tp_bioestadistica.r
@@ -11,9 +11,32 @@ sum(is.na(datos))
 colSums(is.na(datos))
 summary(datos)
 
+
+
+#Estoy viendo las variables en cada columna cualitativa
+unique(datos$rbc)
+unique(datos$pc)
+unique(datos$pcc)
+unique(datos$ba)
+unique(datos$htn)
+unique(datos$dm)
+unique(datos$cad)
+unique(datos$appet)
+unique(datos$pe)
+unique(datos$ane)
+unique(datos$classification)
+
+#Limpieza de data set
+datos[datos == ""] <- NA
+datos$dm <- trimws(datos$dm)
+datos$cad <- trimws(datos$cad)
+datos$classification <- trimws(datos$classification)
+
+#division en enfermos no enfermos
 ckd <- subset(datos, classification == "ckd")
 notckd <- subset(datos, classification == "notckd")
 
+#Ver por enfermedad como estan los datos
 print("Información sobre los datos clasificados con enfermedad renal")
 dim(ckd)
 sum(is.na(ckd))
@@ -29,7 +52,6 @@ sum(is.na(notckd))
 colSums(is.na(notckd))
 na_not <- rowSums(is.na(notckd))
 na_not[na_not != 0]
-class(na_not)
 summary(notckd)
 #Hay que ver que hacer con los NA, podemos ver de poner un numero representativo o dejarlo asi. Si un mismo paciente tiene muchos NA yo lo sacaría.
 
@@ -45,11 +67,27 @@ datos$age_group <- cut(datos$age,
                     labels = paste(seq(1, 91, 10), seq(10, 100, 10), sep = "-"))
 
 
-x11() ; ggplot(datos, aes(x = age_group, fill = classification)) +
-  geom_bar(position = "dodge") +
-  scale_fill_manual(values = c("ckd" = "pink", "notckd" = "purple")) +
+ggplot(datos, aes(x = age_group, fill = classification)) +
+  geom_bar(position = "identity", alpha=0.5) +
+  scale_fill_manual(values = c("ckd" = "red", "notckd" = "purple")) +
   labs(title = "Frecuencia por rango de edad y clasificación",
        x = "Rango de edad",
        y = "Cantidad de pacientes",
        fill = "Clasificación") +
   theme_light()
+
+
+#Histograma bp vs ckd o notcdk
+
+ggplot(datos, aes(x = bp, fill = classification)) +
+  geom_histogram(position = "identity", alpha = 0.5, bins = 50) +
+  scale_fill_manual(values = c("ckd" = "red", "notckd" = "purple")) +
+  scale_x_continuous(breaks = seq(40, 180, 10)) + 
+  labs(title = "Histograma de presión arterial (bp)",
+       x = "Presión arterial",
+       y = "Frecuencia",
+       fill = "Clasificación") +
+  theme_minimal()
+
+
+
